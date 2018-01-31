fabd307e146d2b5d165d070551a79a53387b06da
juarezadolfo <juarezadolfo@hotmail.com>
2018-01-31 13:06:21 -0800

    first commit - install npm packages and creater server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var app = express();
